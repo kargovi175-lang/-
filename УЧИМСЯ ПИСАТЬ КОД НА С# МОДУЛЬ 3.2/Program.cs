6ad846cf5ee76ff357b7a69bd7bfb72779ed437e
Name <email@example.ru>
2026-06-13 16:45:28 +0300

    3

--- УЧИМСЯ ПИСАТЬ КОД НА С# МОДУЛЬ 3.2/Program.cs
+++ УЧИМСЯ ПИСАТЬ КОД НА С# МОДУЛЬ 3.2/Program.cs
@@ -6,12 +6,12 @@ namespace FirstApplication.ConsoleApp
     {
         static void Main(string[] args)
         {
-            const string MyName = "ART";
-            Console.WriteLine(MyName);
-
+            Console.WriteLine(0x00A);
             Console.WriteLine("\t Привет мир");
             Console.WriteLine("\t Мне 22 лет");
             Console.WriteLine("\t MyName name is \n Jane");
+            Console.WriteLine('\u0040');
+            Console.WriteLine('\x23');
             Console.ReadKey();
         }
     }
