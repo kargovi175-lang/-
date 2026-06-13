227067653028c334c06c24290500e3e2d5475c65
Name <email@example.ru>
2026-06-13 15:01:20 +0300

    Добавьте файлы проекта.

---
+++ УЧИМСЯ ПИСАТЬ КОД НА С# МОДУЛЬ 3.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FirstApplication.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Art";
+            Console.WriteLine(MyName);
+
+
+        }
+    }
+}
